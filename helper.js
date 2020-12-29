79585ed3841b75b4e181a0a3182e83d958d825ae
Quentin Gibson <qgibson3@students.kennesaw.edu>
2020-12-29 08:43:26 -0500

    Feat: add item location to popup

--- helper.js
+++ helper.js
@@ -1,7 +1,7 @@
 function listenForClicks() {
 
   document.getElementById('add-js').addEventListener('click', (e) => {
-    e.preventDefault();
+    e.preventDefault()
     document.getElementById('bag-list').innerHTML += `<input class="bolsa" type="text">`
     const elements = document.getElementsByClassName('bolsa')
     elements[elements.length - 1].focus()
@@ -10,9 +10,10 @@ function listenForClicks() {
     let bags = []
 
     function collectMissingItems(bag) {
-      let viewInput = 'test'
-      let css_selector = ''
-      let baseUrl = ''
+      let viewInput = 'changethis'
+      let css_selector = 'changethis'
+      let baseUrl = 'changethis'
+      const regexpLink = /\/inventory\/view_inventory_at\?location_name=.+[0|1]/
 
       // input bag into viewInput value
       content.document.getElementsByName(viewInput).value = bag
@@ -20,23 +21,26 @@ function listenForClicks() {
       content.document.getElementsByName(viewInput).form.submit
       // get item links with yellow backgrounds
       const itemRow = content.document.querySelectorAll(css_selector)
-      const itemText = itemRow.map(item => {
-        return item.innerHTML
-      })
-      const itemLink = itemText.map(text => {
-        const regexp = /\/inventory\/view_inventory_at\?location_name=.+[0|1]/
-        return text.match(regexp)
-
-      })
+      const itemText = itemRow.map(item => item.innerHTML)
+      const itemLink = itemText.map(text => text.match(regexpLink))
       // get all locations from inventory
       for (let link in itemLink) {
         let itemUrl = baseUrl + link
-        browser.tabs.create({
-          active: true,
-          url: itemUrl
-        });
+        browser.tabs.create({active: true, url: itemUrl})
         //get urls from page
-        //close open tabs
+        const trElements = content.document.getElementsByTagName('tr')
+        const trText = trElements.map(element => element.innerHTML)
+
+        const trLinks = trText.map(text => text.match(regexpLink))
+        const locationNames = trLinks.map(link => {
+          return link.substring(link.indexOf('='), link.length - 1)
+        })
+        for (let locationName in locationNames) {
+          const newNameElement = document.createElement('p')
+          newNameElement.innerHTML = locationName
+          document.getElementById('results').appendChild(newNameElement)
+        }
+        //TODO: close open tabs
       }
     }
 
@@ -44,7 +48,7 @@ function listenForClicks() {
 
     const bagElements = document.getElementsByClassName('bolsa')
     for (let bagElement in bagElements) {
-      const value = bagElement.value
+      const value = bagElement.value.trim()
       bags.push(value)
     }
 
